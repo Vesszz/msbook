496d3087dcdfd51a96fb71100b6f3e85bf899b27
Vesszz <tires098@gmail.com>
2024-03-06 17:32:03 +0300

    Added arrow movement

--- main.py
+++ main.py
@@ -7,17 +7,9 @@ import time
 
 #pip install PyQt5
 
-'''
-СДЕЛАТЬ: (возможно)
-выбор книги СДЕЛАЛИ +-
-узнавание главы от индекса
-узнавание страница (в случае .doc/.docx файла)
-процент книги
-'''
-
 global book
 global log 
-log = True #Логгать или нет
+log = False #Логгать или нет
 
 class Book():
     def __init__(self, name: str, author: str, word_index: int, words: list):
@@ -67,35 +59,64 @@ class WordDisplayApp(QMainWindow):
     def showNextWord(self):
         save(book, self.word_index)
         if (self.word_index < len(self.words)):
+            self.word_index += 1
             word = self.words[self.word_index]
             self.label.setText(word)
             font = QFont("Times New Roman", 100) ##
             self.label.setFont(font)
-            self.word_index += 1
             self.timer.start(self.timer_interval)
         else:
             self.stopDisplaying()
 
+    def showWord(self, word_index):
+        save(book, self.word_index)
+        if (word_index < len(self.words)):
+            word = self.words[word_index]
+            self.label.setText(word)
+            font = QFont("Times New Roman", 100) ##
+            self.label.setFont(font)
+        else:
+            self.stopDisplaying()        
+
+    def pause(self):
+        if not self.paused:
+            self.timer.stop()
+            self.paused = True
+        else:
+            self.showNextWord()
+            self.paused = False    
+    
+    def arrow(self, shift):
+        self.timer.stop()
+        self.paused = True
+        self.word_index += shift
+        self.showWord(self.word_index)
+
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Space:
-            if not self.paused:
-                self.timer.stop()
-                self.paused = True
-            else:
-                self.showNextWord()
-                self.paused = False
+            self.pause()
         if event.key() == Qt.Key_F1:
             self.stopDisplaying()
+        if event.key() == Qt.Key_Left:
+            self.arrow(shift=-1)
+        if event.key() == Qt.Key_Right:
+            self.arrow(shift=1)
 
 # надо переработать и не сохранять каждую секунду
 def save(book: Book, word_index: int):
     global log
-    with open('info.txt', 'r', encoding='utf-8') as f:
-        old_data = f.read()
-
+    with open('info.txt', 'r', encoding='utf-8') as f: old_data = f.read()
     new_data = old_data.replace(book.name+";"+book.author+";"+str(word_index-1), book.name+";"+book.author+";"+str(word_index), 1)
     with open('info.txt', 'w', encoding='utf-8') as f: f.write(new_data)
     if log: print("saved")
+
+def append_info(book: Book):
+    global log
+    with open('info.txt', 'r', encoding='utf-8') as f: old_data = f.read()
+    if old_data == "": new_data = book.name+";"+book.author+";-1"
+    else: new_data = old_data + "\n" + (book.name+";"+book.author+";-1")
+    with open('info.txt', 'w', encoding='utf-8') as f: f.write(new_data)
+    if log: print("appended")
 
 def update_info():
 
@@ -107,23 +128,25 @@ def update_info():
         book_names.append(book_info.replace("\n","").split(";")[0])
     for book_name in books:
         if book_name not in book_names:
-            book_author = input("Какой автор книги " + book_name + "?\n")
-            book = Book(book_name,book_author,open('.books/'+book_name, encoding="utf-8").read().split(),0)
-            save(book,0)
+            book_author = input("Кто автор книги " + book_name + "?\n")
+            book = Book(book_name,book_author,open('.books/'+book_name, encoding="utf-8", errors='ignore').read().split(),0)
+            append_info(book)
         
 
 def setup():
     global book
-    #update_info()
+    update_info()
+    book_name_input = input("Введите название книги\n")
+    
     app = QApplication(sys.argv)
     
     books = os.listdir('.books/')
-    book_name_input = input("Введите название книги\n")
+    
     if book_name_input not in books: 
         print("Такой книги на загружено. Добавьте файл в папку /.books")
         return 0
     book_path = '.books/' + book_name_input
-    pre_words = open(book_path, encoding='utf-8').read().split() ## UTF-8
+    pre_words = open(book_path, encoding='utf-8', errors='ignore').read().split() ## UTF-8
     words = []
     i = 0
     while i < len(pre_words):
@@ -146,7 +169,7 @@ def setup():
     info = open("info.txt", encoding="utf-8").readlines()
     for book_info in info:
         if len(book_info.replace("\n","").split(";")) != 3: 
-            print("Неизвестная ошибка")
+            print("В названиях всех сохранённых файлов должен отсуствовать символ ;")
         else: 
             book_name, book_author, book_word_index = book_info.split(";")
             if book_name == book_name_input: 
@@ -157,7 +180,7 @@ def setup():
     
     book = book_selected
     window = WordDisplayApp()
-    interval = 700  # Интервал между словами в миллисекундах
+    interval = 200  # Интервал между словами в миллисекундах
     window.startDisplaying(interval)
     window.show()
     sys.exit(app.exec_())
@@ -170,15 +193,20 @@ if __name__ == '__main__':
     
 '''
 TO DO:
+
+0.НОРМАЛЬНО НАЗВАТЬ ФУНКЦИИ
 1.выбор книги прокруткой + юи
 2.подумать над веб реализацией
 3.оформить паузы
-4.Обработка .docx + .doc + .pdf + других форматов
+4.выбор книги СДЕЛАЛИ +-
+5.узнавание главы от индекса
+6.узнавание страница (в случае .doc/.docx файла)
+7.процент книги
+8.нормальная поддержка .pdf
 
 1. Pyqt5 Scrollarea + dirlist
 2. Если, то хочу Go + React + Микросеть
 3. Отображение части текста при паузе, прокрут мышкой превращает всё в обычный файл с зум ин/аутом
-4. Просто)
 
 сделаем всё будем красавчиками
 '''
